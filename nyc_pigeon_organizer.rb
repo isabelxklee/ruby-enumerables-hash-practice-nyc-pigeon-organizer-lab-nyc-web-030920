01d48f90fcc34af68e5f48b72b6defc16c7e6bed
isabel k. lee <hello@isabelklee.co>
2020-02-20 01:53:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    hash = {}
+    data.each { |key, value|
+        value.each { |k, v|
+            v.each { |element|
+                if !hash[element]
+                  hash[element] = {}
+                end
+
+                if !hash[element][key]
+                  hash[element][key] = []
+                end 
+                hash[element][key].push(k.to_s)
+                
+            }
+        }
+    }
+    hash
 end
